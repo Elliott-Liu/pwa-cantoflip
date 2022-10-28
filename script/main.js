0ecc9407ff03a41b9532c2699e10f77e41590f98
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2022-10-28 15:46:52 +0100

    Add ability to query form elements

--- script/main.js
+++ script/main.js
@@ -1,3 +1,3 @@
 const main = document.querySelector("main");
-const para = document.getElementById("para");
-para.innerText = "We're offline!";
+const translation = document.getElementById("translated-text");
+// translation.value = "The translated text";
